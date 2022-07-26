07e98ff37ae4d881a3d1ce29453f5d27655d8e2f
munenepeter <peternjega7@gmail.com>
2022-07-26 23:02:47 +0300

    the glue of the appliction

---
+++ bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+
+//this file glues the aplication together
+
+require_once __DIR__.'/vendor/autoload.php';
+
+
+//helper functions
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 use DevsTalk\Models\User;
 
-require_once './vendor/autoload.php';
+require_once __DIR__.'/bootstrap.php';
 
 
 var_dump(User::get(988899));
