7a2a4365ec2bc01e899614034647795d5c505596
munenepeter <peternjega7@gmail.com>
2022-08-17 23:12:14 +0300

    working on creating users

--- routes.php
+++ routes.php
@@ -6,3 +6,4 @@ $router->get('api/v1/users', 'UserController@index');
 $router->get('api/v1/users/{id}', 'UserController@show');
 
 $router->post('api/v1/users/signin', 'AuthController@signin');
+$router->post('api/v1/users/create', 'UserController@create');
