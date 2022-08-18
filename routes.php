794ef60918c36849e492b47d7ca3dfee0b9a8372
munenepeter <peternjega7@gmail.com>
2022-08-18 21:51:29 +0300

    Added post routes

--- routes.php
+++ routes.php
@@ -3,9 +3,15 @@
 
 $router->get('', 'ApiController@index');
 $router->get('api/v1', 'ApiController@index');
+
+//users
 $router->get('api/v1/users', 'UserController@index');
 $router->get('api/v1/users/{id}', 'UserController@show');
 
 $router->post('api/v1/user/signin', 'AuthController@signin');
 $router->post('api/v1/user/create', 'UserController@create');
 $router->post('api/v1/user/delete', 'UserController@delete');
+
+//posts
+$router->get('api/v1/posts', 'PostController@index');
+$router->get('api/v1/posts/{id}', 'PostController@show');
